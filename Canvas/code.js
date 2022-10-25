6b0a89baead6a1adee41632f4ab557268fb0342c
blschuldheisz <112977637+blschuldheisz@users.noreply.github.com>
2022-10-25 07:01:09 -0500

    Update code.js

--- Canvas/code.js
+++ Canvas/code.js
@@ -6,10 +6,9 @@ painter1.fillStyle = "#000000";
 painter1.fillRect(0, 0, 400, 400); //x, y, w, h
 
 painter1.fillStyle = "#FF0000";
-
-    for(var j = 0; j < 7; j++) {
-        for(var i = 0; i < 8; i++){
-            painter1.fillRect(10 + (51.5*i), 10 + (60*j), 20, 20)
+    for(var d = 0; d < 7; d++) {
+        for(var c = 0; c < 8; c++){
+            painter1.fillRect(10 + (51.5*c), 10 + (60*d), 20, 20)
         }
     }
     
@@ -23,20 +22,27 @@ painter1.fillStyle = "#FF0000";
 painter2.fillStyle = "#000000";
 painter2.fillRect(0, 0, 400, 400); //x, y, w, h
 
+
 painter2.fillStyle = "#00FF00";
 
-for(x = 0; x < 13; x++){
-    for(y = 0; y < 7; y++){
-        painter2.fillRect((10 + (30*y)) + (30*x), 370 - (30*y), 20, 20);
-    
+x = 0;
+while (x < 13) {
+    y = 0
+    while (y < 7) {
+        if (x<6&&x+y<7) {
+            painter2.fillRect((10 + (30*y)) + (30*x), 370 - (30*y), 20, 20); 
+        } else {
+            if (x>5&&y<4) {
+                painter2.fillRect((10 - (30*y)) + (30*x), 370 - (30*y), 20, 20);
+            } else {
+                if (x>10&&y<6) {
+                    painter2.fillRect((10 - (30*y)) + (30*x), 370 - (30*y), 20, 20);
+                } else {
+                    break;
+                }
+            }
+        }
+        y++;
     }
-}
-
-painter2.fillStyle = "#000000";
-
-for(x = 0; x < 13; x++){
-    for(y = 0; y < 6; y++){
-        painter2.fillRect(370 - (30*y), (340 - (30*y)) - (30*x), 20, 20);
-    
-    }
+    x++;
 }
