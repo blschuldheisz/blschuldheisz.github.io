7b030ac4e773da58050d31b1a45eee9ac7f24884
blschuldheisz <112977637+blschuldheisz@users.noreply.github.com>
2022-10-27 12:37:46 -0500

    Add files via upload

---
+++ Algorithm Swap/algorithm.js
@@ -0,0 +1,13 @@
+var x = 1;
+var y = "1";
+console.log(1 + x + y) //This calls the function 1 + variable x + variable y;
+var t = x;
+x = y;
+y = t;
+alert(1 + x + y);
+/*
+The alert will 
+make a pop-up 
+declaration of 
+"111";
+*/
